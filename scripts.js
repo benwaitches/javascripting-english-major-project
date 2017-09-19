ad02ad76d7312ae03d5d4de2403a7cad66e4f89e
Ben Waitches-Eubanks <benwaitches@172-18-197-9.DYNAPOOL.NYU.EDU>
2017-09-19 18:01:27 -0400

    Exercise 3: Get rid of "this is the #response div"

--- scripts.js
+++ scripts.js
@@ -3,8 +3,3 @@ userInput = prompt("Pick a number, any number...", "Type your answer here.");
 for (let i=1; i<=userInput; i = i + 1 ) {
   $("#response").append("<br />" + i);
 }
-if (userInput%2){
-  $(userInput).append(" is even");
-} else {
-  $(userInput).append(" is odd");
-}
