f49e18c63d635b95d0f3bf476562ff03cb3a1eb9
Ben Waitches-Eubanks <benwaitches@172-18-197-9.DYNAPOOL.NYU.EDU>
2017-09-19 17:59:26 -0400

    Exercise 2: Sad attempt at adding even/odd

--- scripts.js
+++ scripts.js
@@ -3,3 +3,8 @@ userInput = prompt("Pick a number, any number...", "Type your answer here.");
 for (let i=1; i<=userInput; i = i + 1 ) {
   $("#response").append("<br />" + i);
 }
+if (userInput%2){
+  $(userInput).append(" is even");
+} else {
+  $(userInput).append(" is odd");
+}
