1b952a9b024ebef49a4c9737acec78db7298f521
Ben Waitches-Eubanks <benwaitches@172-18-199-102.DYNAPOOL.NYU.EDU>
2017-09-25 23:47:49 -0400

    I mean technically this distinguishes between 0.2 and 20

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 let tipCalculator;
 tipCalculator = function(total, tipRate){
   let tipAmount;
-  tipAmount = tipRate * total;
+  if (tipRate === 0.2)
+  console.log(tipAmount = tipRate * total);
+  if (tipRate === 20)
+  console.log(tipAmount = total * (tipRate/100));
   $("#response").html("Your tip is $" + tipAmount);
 };
 tipCalculator(50.00, 0.2);
