a9568fa3ff28be664700e8c1a0710b68a646cf96
Ben Waitches-Eubanks <benwaitches@172-18-199-102.DYNAPOOL.NYU.EDU>
2017-09-25 23:05:30 -0400

    Created tip calculator

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-let userInput;
-userInput = prompt("Pick a number, any number...", "Type your answer here.");
-for (let i=1; i<=userInput; i = i + 1 ) {
-  $("#response").append("<br />" + i);
-}
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  let tipAmount;
+  tipAmount = tipRate * total;
+  $("#response").html("Your tip is $" + tipAmount);
+};
+tipCalculator(50.00, 0.2);
