716f58d6c8f06159b12d5e3b62593b51879418d9
Ben Waitches-Eubanks <benwaitches@172-18-196-60.DYNAPOOL.NYU.EDU>
2017-10-03 00:30:37 -0400

    Working with objects to calculate burrito spice

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
-let tipCalculator;
-tipCalculator = function(total, tipRate){
-  let tipAmount;
-  if (tipRate === 0.2)
-  console.log(tipAmount = tipRate * total);
-  if (tipRate === 20)
-  console.log(tipAmount = total * (tipRate/100));
-  $("#response").html("Your tip is $" + tipAmount);
-};
-tipCalculator(50.00, 0.2);
+let myHabaneroSauceSquirts, myBurritoObject;
+myHabaneroSauceSquirts = 3;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: myHabaneroSauceSquirts,
+  spiciness: function(){
+    if (myHabaneroSauceSquirts > 0){
+      alert("This is a spicy burrito!");
+    } else {
+      alert("This is a mild burrito.");
+    }
+  }
+  };
+$("#response").html("Your burrito has " + myBurritoObject.habaneroSauceSquirts + " squirts of habanero.");
+myBurritoObject.spiciness();
