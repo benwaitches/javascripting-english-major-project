f1bea671e97bd870ab8da2b52b75099c0ff5a9ae
Ben Waitches-Eubanks <benwaitches@172-18-197-143.DYNAPOOL.NYU.EDU>
2017-10-04 02:08:44 -0400

    Another way to do the last item in the string thing

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 //prompt("The Array Machine requires an array");
-let theArray;
-theArray = (5, 6, true);
-let arrayMachine;
-arrayMachine = function(theArray){
-  let lastItem;
-  lastItem = theArray;
-};
+//let theArray;
+//theArray = (5, 6, true);
+//let arrayMachine;
+//arrayMachine = function(theArray){
+  //let lastItem;
+  //lastItem = theArray;
+//};
 
-$("#response").html(theArray);
+//$("#response").html(theArray);
+let randomArray;
+randomArray = ["Leonardo", "Donatello", "Raphael", "Michelangelo"];
+let lastObject;
+lastObject = randomArray[randomArray.length - 1];
+$("#response").html(lastObject);
