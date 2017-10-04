88ac56d302cd62eaaa1c9de6346f9092d7590f14
Ben Waitches-Eubanks <benwaitches@172-18-197-143.DYNAPOOL.NYU.EDU>
2017-10-04 10:34:49 -0400

    Attempt at "number is interger" function

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
-//prompt("The Array Machine requires an array");
-//let theArray;
-//theArray = (5, 6, true);
-//let arrayMachine;
-//arrayMachine = function(theArray){
-  //let lastItem;
-  //lastItem = theArray;
-//};
-
-//$("#response").html(theArray);
-let randomArray;
-randomArray = ["Leonardo", "Donatello", "Raphael", "Michelangelo"];
-let lastObject;
-lastObject = randomArray[randomArray.length - 1];
-$("#response").html(lastObject);
+let userInput;
+userInput = prompt("Type a number here!");
+Number.isInteger(userInput);{
+  if (userInput.isInteger){
+    alert("This is an integer.");
+  } else{
+    alert("This is not an integer.");
+  }
+}
+$("#response").html(userInput);
