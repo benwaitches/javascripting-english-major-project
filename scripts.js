7beed65edf4e6684d80c826096d10f0d6660a2bd
Ben Waitches-Eubanks <benwaitches@172-18-197-143.DYNAPOOL.NYU.EDU>
2017-10-03 20:59:10 -0400

    Crude function that gives the last number in an array

--- scripts.js
+++ scripts.js
@@ -1,17 +1,10 @@
-let myHabaneroSauceSquirts, myBurritoObject;
-myHabaneroSauceSquirts = 3;
-myBurritoObject = {
-  tortilla: "wheat",
-  guacamole: true,
-  beans: "pinto",
-  habaneroSauceSquirts: myHabaneroSauceSquirts,
-  spiciness: function(){
-    if (myHabaneroSauceSquirts > 0){
-      alert("This is a spicy burrito!");
-    } else {
-      alert("This is a mild burrito.");
-    }
-  }
-  };
-$("#response").html("Your burrito has " + myBurritoObject.habaneroSauceSquirts + " squirts of habanero.");
-myBurritoObject.spiciness();
+//prompt("The Array Machine requires an array");
+let theArray;
+theArray = (5, 6, true);
+let arrayMachine;
+arrayMachine = function(theArray){
+  let lastItem;
+  lastItem = theArray;
+};
+
+$("#response").html(theArray);
