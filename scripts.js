d4484da2af8048aeb2705d45d174049b23451456
Ben Waitches-Eubanks <benwaitches@172-18-195-122.DYNAPOOL.NYU.EDU>
2017-10-11 11:41:45 -0400

    attempt at adding functionality to turtle.weapon function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
-let userArray, arrayDoubled;
-userArray = [1, 4, 8];
-arrayDoubled = userArray.map(function(userArray){
-  return userArray * 2;
-}).join(", ");
-$("#response").html(arrayDoubled);
+let leonardo, donatello, raphael, michelangelo, turtles, weapons, arrayLength, addAnd;
+leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
+donatello = {name: "Donatello", color: "purple", weapon: "bo"};
+raphael = {name: "Raphael", color: "red", weapon: "sai"};
+michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
+turtles = [leonardo, donatello, raphael, michelangelo];
+weapons = turtles.map(function(turtle){
+  arrayLength = weapons.length;
+  //my goal here is to somehow show the program exactly where to put the "and"
+  addAnd = weapons.length - 1;
+  //I thought that maybe by finding the length and then subtracting one, the program would know which spot I was after
+  weapons.join("and");
+  //This obviously didn't work because I don't think i've actually identified the spot, and selected it in the array.
+  return turtle.weapon;
+}).sort().join(", ");
+$("#response").html(weapons);
