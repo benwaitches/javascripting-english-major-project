cb35d677d4a8e58d3ef1c2bbb0e8ef92a7e8f808
Ben Waitches-Eubanks <benwaitches@172-18-195-122.DYNAPOOL.NYU.EDU>
2017-10-11 11:22:42 -0400

    .map function that doubles an array of numbers

--- scripts.js
+++ scripts.js
@@ -1,17 +1,6 @@
-let userString, upperCaseMinusE, upperCasedString;
-userString = prompt("What do you want to UPPeRCASe?");
-upperCaseMinusE = function(string){
-  let result, stringArray;
-  result = " ";
-  stringArray = string.split("");
-  stringArray.forEach(function(letter){
-    if(letter ==="e"){
-      result = result + letter;
-    } else {
-      result = result + letter.toUpperCase();
-    }
-  });
-  return result;
-};
-upperCasedString = upperCaseMinusE(userString);
-$("#response").html(upperCasedString);
+let userArray, arrayDoubled;
+userArray = [1, 4, 8];
+arrayDoubled = userArray.map(function(userArray){
+  return userArray * 2;
+}).join(", ");
+$("#response").html(arrayDoubled);
