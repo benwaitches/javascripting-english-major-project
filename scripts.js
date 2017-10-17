e061126f88264db10b074c3e390b57a2cd49b7f6
Ben Waitches-Eubanks <benwaitches@172-18-193-2.DYNAPOOL.NYU.EDU>
2017-10-16 21:19:35 -0400

    making mistakes homework

--- scripts.js
+++ scripts.js
@@ -1,16 +1,9 @@
-let leonardo, donatello, raphael, michelangelo, turtles, weapons, arrayLength, addAnd;
-leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
-donatello = {name: "Donatello", color: "purple", weapon: "bo"};
-raphael = {name: "Raphael", color: "red", weapon: "sai"};
-michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
-turtles = [leonardo, donatello, raphael, michelangelo];
-weapons = turtles.map(function(turtle){
-  arrayLength = weapons.length;
-  //my goal here is to somehow show the program exactly where to put the "and"
-  addAnd = weapons.length - 1;
-  //I thought that maybe by finding the length and then subtracting one, the program would know which spot I was after
-  weapons.join("and");
-  //This obviously didn't work because I don't think i've actually identified the spot, and selected it in the array.
-  return turtle.weapon;
-}).sort().join(", ");
-$("#response").html(weapons);
+let turtlesWithSplinter, turtlesWithoutSplinter, reversedTurtlesWithoutSplinter;
+turtlesWithSplinter = ["Leonardo", "Donatello", "Raphael", "Michelangelo", "Splinter"];
+reversedTurtlesWithoutSplinter = turtlesWithSplinter.filter(function(turtle){
+  console.log(turtle);
+  return turtle !== "Splinter";
+}).reverse();
+$("#response").html(reversedTurtlesWithoutSplinter);
+//reversedTurtlesWithoutSplinter = turtlesWithSplinter.pop().reverse();
+//$("#response").html(reversedTurtlesWithoutSplinter);
