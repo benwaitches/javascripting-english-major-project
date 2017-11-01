882c513843499689dfef7b6dcb04e6bf19922446
Ben Waitches-Eubanks <benwaitches@172-18-194-128.DYNAPOOL.NYU.EDU>
2017-11-01 11:20:26 -0400

    Ex 3: attempted to add wikipedia links

--- prologue.js
+++ prologue.js
@@ -12,6 +12,10 @@ $.getJSON("https://the-javascripting-english-major.org/prologue.json", function(
       if (word.modern){
         wordString = "<a href='#' data-modern='" + word.modern + "'>" + wordString + "</a>";
       }
+      if (word.url){
+        wordString = "<a href='#' data-url='" + word.url + "'>" + wordString + "</a>";
+        //added an attribute so the program can look for words with a wiki url
+      }
       lineText = lineText + wordString + " ";
     });
     prologueText = prologueText + lineText + "<br/>";
@@ -19,10 +23,13 @@ $.getJSON("https://the-javascripting-english-major.org/prologue.json", function(
   prologueText = prologueText + "</p></blockquote>";
   $("#prologue").html(prologueText);
   $("#prologue a").click(function(){
-    let glossText, clickedWord, modernWord;
+    let glossText, clickedWord, modernWord, wikiUrl;
     clickedWord = $( this ).text();
     modernWord = $( this ).data("modern");
-    glossText = "<h2>You clicked on " + clickedWord + ", which means " + modernWord +"</h2>";
+    wikiUrl = $( this ).data("url");
+    //if thwy click, the function will check for a url
+    glossText = "<h2>You clicked on " + clickedWord + ", which means " + modernWord + ". Would you like to learn more?" + wikiUrl +"</h2>";
+    //this didn't actually work, tried adding something like "<a href=wikiUrl> Click here</a>", but this also didn't work. I'm not sure why this is undefined.
     $("#glosses").html(glossText);
   });
 });
