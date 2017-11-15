9c3dae6895daaa615106bfad766d33fc2757991e
Ben Waitches-Eubanks <benwaitches@172-18-193-169.DYNAPOOL.NYU.EDU>
2017-11-15 00:39:15 -0500

    map with data for "could-be"

--- could-be.js
+++ could-be.js
@@ -6,3 +6,25 @@ tileLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/ligh
               maxZoom: 18
             }).addTo(map);
 map.setView([40.730833, -73.9975], 16);
+let couldBeFeatures;
+$.getJSON("https://the-javascripting-english-major.org/could-be.geo.json", function(data){
+  let couldBeLayer;
+  couldBeFeatures = data.features.map(function(feature){
+    return {
+      name: feature.properties.name,
+      html: feature.properties.html,
+      tab: feature.properties.tab,
+      mentions: feature.properties.mentions,
+      lines: feature.properties.lines,
+      wikipedia: feature.properties.wikipedia,
+      latLng: L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
+    };
+  });
+  couldBeLayer = L.featureGroup(couldBeFeatures.map(function(feature){
+    return L.marker(feature.latLng);
+    })
+  );
+  couldBeLayer.addTo(map);
+  map.fitBounds(couldBeLayer.getBounds());
+  map.zoomOut(1);
+});
