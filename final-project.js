84c0bfdcf90a8cfb8796a9c4097cdfc1071dded4
Ben Waitches-Eubanks <benwaitches@172-18-192-123.DYNAPOOL.NYU.EDU>
2017-11-20 01:48:31 -0500

    attempt at adding a #content div to put the text on the page

--- final-project.js
+++ final-project.js
@@ -1,12 +1,3 @@
-let map, tileLayer;
-map = L.map("final-project-map");
-tileLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
-              attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
-              subdomains: "abcd",
-              maxZoom: 18
-            }).addTo(map);
-map.setView([40.730833, -73.9975], 16);
-
 let md;
 md = window.markdownit({html: true}).use(window.markdownitFootnote);
 $.ajax({
@@ -17,3 +8,11 @@ $.ajax({
     $("#content").html(html);
   }
 });
+let map, tileLayer;
+map = L.map("final-project-map");
+tileLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+              attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
+              subdomains: "abcd",
+              maxZoom: 18
+            }).addTo(map);
+map.setView([40.730833, -73.9975], 16);
