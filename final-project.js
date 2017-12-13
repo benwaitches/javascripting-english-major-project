12f883896055658960894e495687fee0d31ce024
Ben Waitches-Eubanks <benwaitches@172-18-195-238.DYNAPOOL.NYU.EDU>
2017-12-12 21:55:25 -0500

    attempting to add a second markdown file

--- final-project.js
+++ final-project.js
@@ -1,8 +1,8 @@
 let md;
 md = window.markdownit({html: true}).use(window.markdownitFootnote);
-["introduction"].forEach(function(tab){
+["introduction", "brussels-airport"].forEach(function(tab){
   $.ajax({
-    url: "https://benwaitches.github.io/javascripting-english-major-project/final-project.html" + tab + ".md",
+    url: "https://benwaitches.github.io/javascripting-english-major-project/" + tab + ".md",
     success: function(markdown){
       let html;
       html = md.render(markdown);
@@ -10,11 +10,18 @@ md = window.markdownit({html: true}).use(window.markdownitFootnote);
     }
   });
 });
-let map, tileLayer;
-map = L.map("final-project-map");
-tileLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+let mapNYC, mapBrussels, tileLayerNYC, tileLayerBrussels;
+mapNYC = L.map("final-project-map-nyc");
+tileLayerNYC = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
               attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
               subdomains: "abcd",
               maxZoom: 18
-            }).addTo(map);
-map.setView([40.730833, -73.9975], 16);
+            }).addTo(mapNYC);
+mapNYC.setView([40.730833, -73.9975], 13);
+mapBrussels = L.map("final-project-map-brussels");
+tileLayerBrussels = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+              attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
+              subdomains: "abcd",
+              maxZoom: 18
+            }).addTo(mapBrussels);
+mapBrussels.setView([50.848821, 4.352118], 13);
